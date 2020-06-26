3daaa2294c2ee501a584323054de283df6ae0f3b
mlcode1 <837083114@qq.com>
2020-06-26 18:05:52 +0800

    package.json

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case',() => {
     const baseConfig = require('../../lib/webpack.base')
     // console.log(baseConfig)
     it('entry',() => {
-        assert.equal(baseConfig.entry.index,'D:/webstorm/basic/ehsplus-webpack/test/smoke/template/src/js/index.js');
-        assert.equal(baseConfig.entry.search,'D:/webstorm/basic/ehsplus-webpack/test/smoke/template/src/js/search.js');
+        assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/js/index.js') > -1,true);
+        assert.equal(baseConfig.entry.search.indexOf('test/smoke/template/src/js/search.js') > -1,true);
     })
 })
